b2219cf329b01267046cb5cfbdcf0e8474c80492
Clemens Eyhoff <clemens.eyhoff@fit.fraunhofer.de>
2025-05-29 12:57:19 +0200

    made clang happy

--- include/config.h
+++ include/config.h
@@ -6,7 +6,7 @@
 //--------------------------------------------------------------------------
 // PIN Configuration
 //--------------------------------------------------------------------------
-#define LED_PIN 18   //18 = D3 or 0     // Use direct pin number
+#define LED_PIN 18       // 18 = D3     // Use direct pin number
 #define SDA_PIN_ESP32 21 // Use direct pin number
 #define SCL_PIN_ESP32 22 // Use direct pin number
 
